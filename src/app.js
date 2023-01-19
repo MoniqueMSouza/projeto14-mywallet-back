ef1f830d1a61404b678f70070f824e0c22fb2651
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-01-19 11:41:10 -0300

    Conectando ao Banco de dados

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import express from "express";
 import cors from "cors";
 import chalk from "chalk"
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv'
 
+
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+try {
+  mongoClient.connect()
+  db = mongoClient.db();
+  console.log('Conectou com o mongodb!')
+} catch (error) {
+  console.log('Deu erro no banco de dados!')
+}
 
 
 const app = express();
